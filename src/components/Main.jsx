8f59ccdb81d42d4e846fcd3e9a214bd937684978
Wan Muzaffar <wanmuz86@gmail.com>
2024-06-15 15:54:52 +0800

    onclick and style

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 
 const Main = () => {
   return (
-    <div>
+    <div style={{padding:'1.5rem', backgroundColor:'yellow'}}>
         <h1>Hello World</h1>
         <p>Welcome to my first React.js project</p>   
         <hr/>
-        <h2>Who are you?</h2>
+        <h2 style={{fontSize:'1.5rem'}}>Who are you?</h2>
         <div>
             <label htmlFor='name'>Name</label>
             <input type="text" id="name" placeholder='Enter your name'/>
+            <button onClick={()=> alert("Data sent!")}>Send</button>
         </div>
     </div>
     
