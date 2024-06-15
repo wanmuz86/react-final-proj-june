02fbb7629096cd9e8ff672bac2e99939a802ed66
Wan Muzaffar <wanmuz86@gmail.com>
2024-06-15 16:25:37 +0800

    add message variable to revise useState

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,15 +7,30 @@ const Main = () => {
     // When we want to update the variable/the state use setNumber method (setter)
 
     const [number, setNumber] = useState(0)
+    const [message,setMessage] = useState("")
 
     // Create a function using () =>
     // THis function is called incrementNumber
     //It will set the number to number+1 // number = number+1, number++
-    const incrementNumber = () => setNumber(number+1)
+    const incrementNumber = () => {
+        setNumber(number+1)
+        setMessage("")
+    }
 
-    const decrementNumber = () =>setNumber(number-1)
+    const decrementNumber = () =>{
+        if (number == 0) {
+            setMessage("Number cannot be less than 0")
+        } 
+        else {
+            setNumber(number-1)
+        }
+       
+    }
 
-    const reset = () => setNumber(0)
+    const reset = () => {
+        setNumber(0)
+        setMessage("")
+    }
 
     
 
@@ -34,6 +49,7 @@ const Main = () => {
         <hr/>
         <h2>Counter</h2>
         <h4>{number}</h4>
+        <p style={{color:'red'}}>{message}</p>
         <button onClick={incrementNumber}>Increment number</button>
         <button onClick={decrementNumber}>Decrement number</button>
         <button onClick={reset}>Reset to Zero</button>
