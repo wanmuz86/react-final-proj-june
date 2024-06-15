0964ee6788f794ce508fa23d1933d5610961d2fd
Wan Muzaffar <wanmuz86@gmail.com>
2024-06-15 15:49:26 +0800

    usage of htmlFor

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import '../App.css'
 
 const Footer = () => {
   return (
-    <footer>Footer</footer>
+    <footer className='footer'>Footer</footer>
   )
 }
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,6 +5,12 @@ const Main = () => {
     <div>
         <h1>Hello World</h1>
         <p>Welcome to my first React.js project</p>   
+        <hr/>
+        <h2>Who are you?</h2>
+        <div>
+            <label htmlFor='name'>Name</label>
+            <input type="text" id="name" placeholder='Enter your name'/>
+        </div>
     </div>
     
   )
