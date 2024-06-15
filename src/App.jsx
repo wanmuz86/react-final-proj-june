149b6af2d19eda78aa59e6aa508e9355dc8a9c02
Wan Muzaffar <wanmuz86@gmail.com>
2024-06-15 15:05:56 +0800

    first part: creating componets

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
+
 function App() {
 
 
   return (
     <>
-     <h1>Hello World</h1>
+     <Header/>
+     <Main/>
+     <Footer/>
     </>
   )
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div>
+        <h1>Hello World</h1>
+        <p>Welcome to my first React.js project</p>
+        </div>
+  )
+}
+
+export default Main
