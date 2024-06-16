3a25224f9636286e7d262b9b227422eb21ac8e3a
Wan Muzaffar <wanmuz86@gmail.com>
2024-06-16 09:58:37 +0800

    Move Counter into it's own component

---
+++ src/components/Counter.jsx
@@ -0,0 +1,47 @@
+import React,{useState} from 'react'
+
+const Counter = () => {
+
+     // Create a variable/state number
+    // The initial value of this variable/the state is 0 (getter)
+    // When we want to update the variable/the state use setNumber method (setter)
+
+    const [number, setNumber] = useState(0)
+    const [message,setMessage] = useState("")
+
+    // Create a function using () =>
+    // THis function is called incrementNumber
+    //It will set the number to number+1 // number = number+1, number++
+    const incrementNumber = () => {
+        setNumber(number+1)
+        setMessage("")
+    }
+
+    const decrementNumber = () =>{
+        if (number == 0) {
+            setMessage("Number cannot be less than 0")
+        } 
+        else {
+            setNumber(number-1)
+        }
+       
+    }
+
+    const reset = () => {
+        setNumber(0)
+        setMessage("")
+    }
+
+  return (
+    <div>
+   
+        <h4>{number}</h4>
+        <p style={{color:'red'}}>{message}</p>
+        <button onClick={incrementNumber}>Increment number</button>
+        <button onClick={decrementNumber}>Decrement number</button>
+        <button onClick={reset}>Reset to Zero</button>
+    </div>
+  )
+}
+
+export default Counter
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,39 +1,8 @@
-import React,{useState} from 'react'
-
+import React from 'react'
+import Counter from './Counter'
 const Main = () => {
 
-    // Create a variable/state number
-    // The initial value of this variable/the state is 0 (getter)
-    // When we want to update the variable/the state use setNumber method (setter)
-
-    const [number, setNumber] = useState(0)
-    const [message,setMessage] = useState("")
-
-    // Create a function using () =>
-    // THis function is called incrementNumber
-    //It will set the number to number+1 // number = number+1, number++
-    const incrementNumber = () => {
-        setNumber(number+1)
-        setMessage("")
-    }
-
-    const decrementNumber = () =>{
-        if (number == 0) {
-            setMessage("Number cannot be less than 0")
-        } 
-        else {
-            setNumber(number-1)
-        }
-       
-    }
-
-    const reset = () => {
-        setNumber(0)
-        setMessage("")
-    }
-
-    
-
+   
   return (
     <div style={{padding:'1.5rem', backgroundColor:'yellow'}}>
         <h1>Hello World</h1>
@@ -48,11 +17,8 @@ const Main = () => {
 
         <hr/>
         <h2>Counter</h2>
-        <h4>{number}</h4>
-        <p style={{color:'red'}}>{message}</p>
-        <button onClick={incrementNumber}>Increment number</button>
-        <button onClick={decrementNumber}>Decrement number</button>
-        <button onClick={reset}>Reset to Zero</button>
+        <Counter/>
+        
     </div>
     
   )
