c507e869475290056fcbb70b2ffbbb5d44da3948
Wan Muzaffar <wanmuz86@gmail.com>
2024-06-16 11:36:21 +0800

    add user steps logic

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Counter from './Counter'
 import UserInfo from './UserInfo'
 
@@ -28,6 +28,30 @@ const Main = () => {
         }
     ]
 
+    // 1 ) Create State to manage the input of the form
+
+    const [userForm, setUserForm] = useState({"name":"", "age":0})
+
+    // 2) Create methods to handle change on the inputs
+
+    const handleUserNameChanged = (e) => {
+    
+        // Update the value name in the state userForm to the updated name
+        // ... (spread operator)
+        // e.target.value refer to the value entered by user
+        setUserForm({...userForm, name:e.target.value})
+
+    }
+
+    const handleAgeChanged = (e) => {
+        setUserForm({...userForm, age:e.target.value })
+
+    }
+
+    const handleUserPressed = () => {
+        console.log(userForm)
+    }
+
   return (
     <div style={{padding:'1.5rem', backgroundColor:'yellow'}}>
         <h1>Hello World</h1>
@@ -50,13 +74,23 @@ const Main = () => {
         <UserInfo user={mainUser}/>
 
         <hr/>
-        
+
         <h2>Add User Form</h2>
+
         <label htmlFor='userName'>User Name</label>
-        <input type="text" id='userName' placeholder='Enter user name'/>
+
+        {/* 3) Link  the value and onChange to the created state and methods*/}
+
+        <input type="text" id='userName' placeholder='Enter user name' 
+        onChange={handleUserNameChanged}
+        value={userForm.name} />
         <label htmlFor='userAge'>User Age</label>
-        <input type="text" id='userAge' placeholder='Enter user age' />
-        <button>Add User</button>
+        <input type="text" id='userAge' placeholder='Enter user age' 
+        onChange={handleAgeChanged}
+        value={userForm.age} />
+          {/* 4) Call the method handleUserPressed*/}
+        
+        <button onClick={handleUserPressed}>Add User</button>
 
         <hr />
         <h2>Users information</h2>
